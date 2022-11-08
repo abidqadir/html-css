a14580a074b06b93fa166e878a24a2c4778e5b21
Abid Qadir <Abidumrani222@gmail.com>
2022-11-08 19:46:02 +0500

    starting js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let num = [2,3,4]
-let sum =1
-for ( let i = 0; i<num.length;i++)
-{
-    sum*= num[i]
+let num1 = 0
+let num2 = 0
+let sum = 0
+console.log("write a first number ", num1)
+console.log("write a second number ", num2)
+sum = num1 + num2
+console.log("the sum is ", sum)
 
-}
-
-console.log(sum)
