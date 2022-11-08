0e87189e548a0434ebe60bf752a65303dd2abffa
Abid Qadir <Abidumrani222@gmail.com>
2022-11-08 21:02:10 +0500

    starting js

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-let num1 = 0
-let num2 = 0
-let sum = 0
-console.log("write a first number ", num1)
-console.log("write a second number ", num2)
-sum = num1 + num2
-console.log("the sum is ", sum)
+function calculator (num1 ,num2,operator)
+{
 
+if (operator === "+")
+{
+ return num1+num2
+ 
+}
+
+else if(operator === "-") 
+{
+    return num1-num2
+}
+
+else if(operator === "*") 
+{
+    return num1*num2
+}
+
+else if(operator === "/") 
+{
+    return num1/num2
+}
+
+
+
+
+else {
+    return "wrong oprt"
+}
+}
+
+console.log(calculator(0,5,"-"))
